87e762134ed7bdc14896ac894babb0a4e3e6196f
absolom <olivier@make-it.fr>
2020-07-25 13:30:54 +0200

    config eslint and husky

--- index.js
+++ index.js
@@ -1,82 +1,83 @@
 class CondorcetRandomized {
-  candidates = [];
-  votes = [];
-  matrix = [];
+  constructor () {
+    this.candidates = []
+    this.votes = []
+    this.matrix = []
+  }
 
-  addCandidate(candidate) {
+  addCandidate (candidate) {
     if (typeof candidate !== 'string' || candidate.length === 0) {
-      throw new Error('"candidate" must be a non empty string');
+      throw new Error('"candidate" must be a non empty string')
     }
 
     if (this.candidates.indexOf(candidate) !== -1) {
-      throw new Error('"' + candidate + '" is allredy in candidates');
+      throw new Error('"' + candidate + '" is allredy in candidates')
     }
 
-    const total =  this.candidates.push(candidate);
+    const total = this.candidates.push(candidate)
 
-    return total - 1;
+    return total - 1
   }
 
-  addVote(vote) {
+  addVote (vote) {
     if (!Array.isArray(vote)) {
-      throw new Error('vote must be a non empty array');
+      throw new Error('vote must be a non empty array')
     }
 
     if (vote.length > this.candidates.length) {
-      throw new Error('vote is more large of candidates list');
+      throw new Error('vote is more large of candidates list')
     }
 
     if (new Set(vote).size !== vote.length) {
-      throw new Error('vote have duplicates votes');
+      throw new Error('vote have duplicates votes')
     }
 
     vote.forEach((candidate) => {
-      if (typeof candidate !== 'number'
-      || candidate < 0
-      || candidate >= this.candidates.length) {
-        throw new Error('vote item must be an candidate index');
+      if (typeof candidate !== 'number' ||
+      candidate < 0 ||
+      candidate >= this.candidates.length) {
+        throw new Error('vote item must be an candidate index')
       }
-    });
+    })
 
-    this.votes.push(vote);
+    this.votes.push(vote)
   }
 
-  resolveDuel(a, b) {
+  resolveDuel (a, b) {
     const score = this.votes.reduce((accu, vote) => {
-      const scoreA = vote.indexOf(a);
-      const scoreB = vote.indexOf(b);
+      const scoreA = vote.indexOf(a)
+      const scoreB = vote.indexOf(b)
 
       if (scoreA === -1 || scoreB === -1) {
-        return accu;
+        return accu
       }
 
-      return accu + Math.sign(scoreB - scoreA);
-    }, 0);
+      return accu + Math.sign(scoreB - scoreA)
+    }, 0)
 
-    const result = Math.sign(score);
+    const result = Math.sign(score)
 
     if (!Array.isArray(this.matrix[a])) {
-      this.matrix[a] = [];
+      this.matrix[a] = []
     }
     if (!Array.isArray(this.matrix[b])) {
-      this.matrix[b] = [];
+      this.matrix[b] = []
     }
 
-    this.matrix[b][a] = -result;
+    this.matrix[b][a] = -result
     this.matrix[a][b] = result
   }
 
-  generateLottery() {
+  generateLottery () {
     this.candidates.forEach((a, i) => {
       this.candidates.forEach((b, j) => {
         if (!this.matrix[i] || !this.matrix[i][j]) {
-          this.resolveDuel(i, j);
+          this.resolveDuel(i, j)
         }
-      });
-    });
-    console.log(this.matrix);
+      })
+    })
+    console.log(this.matrix)
   }
 };
 
-module.exports = CondorcetRandomized;
-
+module.exports = CondorcetRandomized
--- tests/sample.js
+++ tests/sample.js
@@ -1,15 +1,15 @@
 #!/bin/node
 
-const CondorcetRandomized = require('./index.js');
+const CondorcetRandomized = require('./index.js')
 
-const c = new CondorcetRandomized();
+const c = new CondorcetRandomized()
 
-const candidateA = c.addCandidate('A');
-const candidateB = c.addCandidate('B');
-const candidateC = c.addCandidate('C');
+const candidateA = c.addCandidate('A')
+const candidateB = c.addCandidate('B')
+const candidateC = c.addCandidate('C')
 
-c.addVote([ candidateA, candidateB, candidateC ]);
-c.addVote([ candidateB, candidateC, candidateA ]);
-c.addVote([ candidateC, candidateA, candidateB ]);
+c.addVote([candidateA, candidateB, candidateC])
+c.addVote([candidateB, candidateC, candidateA])
+c.addVote([candidateC, candidateA, candidateB])
 
-c.generateLottery();
+c.generateLottery()
